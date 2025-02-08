e270fe5e539e861ae15315c19afa48fa8246f382
emilyarana23 <earana382@gmail.com>
2025-02-07 21:59:46 -0600

    Initial commit

---
+++ info/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Pagina Web Emily</title>
+</head>
+<body>
+<header> 
+    <nav>  
+        <a href="#">Inicio</a>
+        <a href="#">Acerca de</a>
+        <a href="#">Portafolio</a>
+        <a href="#">Servicios</a>
+        <a href="#">Contacto</a>
+    </nav> 
+    <section class="textos-header">
+        <h1>Quinto Informática 2025</h1>
+        <h2>Práctica de Taller</h2>
+    </section>
+</header>
+<main>
+    <section class="contenedor sobre-nosotros">
+        <h2 class="titulo">Nuestro producto</h2>
+        <div class="contenedor-sobre-nosotros">
+            <img src="img/capibara1.jpg" alt="" class="imagen-about-us">
+            <div class="contenido-texto">
+                <h3><span>1</span> Los Mejores Productos</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nulla enim quam provident facilis excepturi vitae blanditiis veritatis, modi veniam explicabo pariatur sint voluptatem molestias dolores laudantium optio quisquam maiores!</p>
+                <h3><span>2</span> Los Mejores Productos</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis officia placeat vitae cupiditate velit, nihil quam facilis quos est, inventore exercitationem reprehenderit eaque rerum sunt molestiae pariatur. Iure, commodi fugit.</p>
+            </div>
+        </div>
+    </section>
+    <section class="portafolio">
+        <div class="contenedor">
+            <h2 class="titulo">Portafolio</h2>
+            <div class="galeria-port">
+                <div class="imagen-port">
+                    <img src="img/capibara2.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/capibara3.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/capibara4.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/capybara5.webp" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/youg.webp" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/capibara7.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/capibara8.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="img/capibara9.jpg" alt="">
+                    <div class="hover-galeria">
+                        <img src="img/signal_12534276.png" alt="">
+                        <p>Nuestro Trabajo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="clientes contenedor">
+        <h2 class="titulo">Qué dicen nuestros clientes</h2>
+        <div class="cards">
+            <div class="card">
+                <img src="img/rostro.jpg" alt="">
+                <div class="contenido-texto-card">
+                    <h4>Nombre</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit doloribus harum consequuntur ullam, neque maxime quis labore explicabo! Dolorem.</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="img/images.jfif" alt="">
+                <div class="contenido-texto-card">
+                    <h4>Nombre</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit doloribus harum consequuntur ullam, neque maxime quis labore explicabo! Dolorem.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main> <!-- Cierre correcto del main -->
+
+<footer>
+    <div class="footer-container">
+        <div class="footer-item">
+            <strong>TEL:</strong> <br>
+            5030-5687
+        </div>
+        <div class="footer-item">
+            <strong>Email:</strong> <br>
+            emilyarroyo@cetach2.edu.gt
+        </div>
+        <div class="footer-item">
+            <strong>UBICACIÓN:</strong> <br>
+            4ta calle 2-31 zona 3 zara
+        </div>
+    </div>
+    <p class="footer-copy">©EMILY ARANA | CETACH-2025</p>
+</footer>
+
+</body>
+</html>
